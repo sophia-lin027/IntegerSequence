cdec25438171b442a9753b11a8250e7dbac590d2
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-12-06 14:35:35 -0500

    added length

--- Range.java
+++ Range.java
@@ -23,19 +23,23 @@ public class Range implements IntegerSequence{
   }
 
   public int length(){
-
+    int count = 0;
+    for (int i = start; i < end + 1; i++) {
+      count++;
+    }
+    return count;
   }
 
   //When current is no longer a valid element in the range, it should return false.
   public boolean hasNext(){
-
+    return true;
   }
 
   //@throws NoSuchElementException when hasNext() is false.
   //This will return the current value, it will also increase current value by 1.
   //e.g.  if current is 5. This will make current 6, and return 5.
   public int next(){
-
+    return 1;
   }
 
 }
