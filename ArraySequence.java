f01f7ce7111c76f48b527d9b3ffd360c73df5b3a
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-12-07 13:38:53 -0500

    new file

---
+++ ArraySequence.java
@@ -0,0 +1,17 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  int currentIndex;
+  int []data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){
+
+  }
+
+  //Postcondition: The otherseq will be reset.
+  //This constructor will copy ALL values of the `otherseq` into the data array.
+
+  public ArraySequence(IntegerSequence otherseq){
+    
+  }
+}
